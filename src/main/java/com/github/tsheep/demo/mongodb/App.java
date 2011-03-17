89dd95a6f1056b7df4c7692071fe78bd47200d35
Eric Lee <tsheep.lee@gmail.com>
2011-03-17 22:29:04 +0800

    first commit

---
+++ src/main/java/com/github/tsheep/demo/mongodb/App.java
@@ -0,0 +1,13 @@
+package com.github.tsheep.demo.mongodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
